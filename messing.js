3e2bc6c953d3bce0eaa8f18711799456eaec7f30
Harsh Pandey <hpandey793@gmail.com>
2014-09-15 21:45:13 -0400

    removed unused files

--- messing.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var express = require('express');
-var request = require('request');
-var http = require('http');
-var gm = require('gm');
-var spawn = require('child_process').spawn;
-
-gm('contact.jpg').write('contact.png', function (err) {});
--- testtarget.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('*', function (req, res) {
-  console.log(req.url);
-  res.end('hello');
-});
-
-app.listen(3000);
